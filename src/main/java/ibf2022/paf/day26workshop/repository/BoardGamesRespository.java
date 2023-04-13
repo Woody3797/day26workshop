0d57ce8eb19a04ee1e8d22864253000ee7532762
Woody3797 <woody3797@gmail.com>
2023-04-13 14:49:45 +0800

    day26workshop

--- src/main/java/ibf2022/paf/day26workshop/repository/BoardGamesRespository.java
+++ src/main/java/ibf2022/paf/day26workshop/repository/BoardGamesRespository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
